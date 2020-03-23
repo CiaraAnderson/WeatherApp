15d980afffcd35c12f50488a74b4caf3c71a8a7a
Ciara Anderson <thelaylinguist@gmail.com>
2020-03-23 09:38:48 -0700

    Added Forecast and Geocode Modules

--- app.js
+++ app.js
@@ -1,38 +1,28 @@
 const request = require('request')
-
-// const url = 'https://api.darksky.net/forecast/087f3f5563f6142cd4eab9fabd3477e1/37.8267,-122.4233'
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+// const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/37.8267,-112'
 
 // request({ url: url, json: true }, (error, response) => {
-    
 //     if (error) {
-//     console.log ('Unable to connect')
-
+//         console.log('Unable to connect to weather service!')
 //     } else if (response.body.error) {
-//             console.log('Unable to find location')
-
-//     } else {console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degrees out. There is ' + response.body.currently.precipProbability + '% chance of rain')
-
+//         console.log('Unable to find location')
+//     } else {
+//         console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degress out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')   
 //     }
-    
-    
 // })
 
 
 
-const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiY2lhcmFhbmRlcnNvbiIsImEiOiJjazgwa3Q0ZjQwZ2o1M2xyeXB1YWJzYzExIn0.gYDD5aJ3DQiWGXDpIDDV5Q&limit=1'
-
-request ( { url: geocodeURL, json: true }, (error, response) => {
-    
-
-    if (error) {
-        console.log ('Unable to connect')
-        
-    } else if (response.body.features.length === 0 ) {
-         console.log('Unable to find location')
-    } else {
-    const latitude = response.body.features[0].center[1]
-    const longitude = response.body.features[0].center[0]
-    console.log(latitude, longitude)}
 
 
-})
+// geocode('Boston', (error, data) => {
+//     console.log('Error', error)
+//     console.log('Data', data)
+// })
+
+forecast(-75.7088, 44.1545, (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
+  })
