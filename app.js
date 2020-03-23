66159918532b0494df9d3cd1e8e0f94d4ff47abc
Ciara Anderson <thelaylinguist@gmail.com>
2020-03-23 12:09:41 -0700

    Callback Chaining

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
+const address = process.argv[2]
 
+if (!address) {
+    console.log('Please provide an address')
+} else {
+    geocode(address, (error, data) => {
+        if (error) {
+            return console.log(error)
+        }
 
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
 
-
-geocode('Boston', (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-})
-
-forecast(-75.7088, 44.1545, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-  })
+            console.log(data.location)
+            console.log(forecastData)
+        })
+    })
+}
--- utils/forecast.js
+++ utils/forecast.js
@@ -1,22 +1,17 @@
 const request = require('request')
 
-
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/087f3f5563f6142cd4eab9fabd3477e1/37' + latitude + ',' + longitude 
+    const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
 
     request({ url: url, json: true }, (error, response) => {
         if (error) {
-            callback ('Unable to connect', undefined)
-
+            callback('Unable to connect to weather service!', undefined)
         } else if (response.body.error) {
             callback('Unable to find location', undefined)
-
-        } else {callback(undefined, response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degrees out. There is ' + response.body.currently.precipProbability + '% chance of rain')
-
-    }
+        } else {
+            callback(undefined, response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degress out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
+        }
     })
-    
-    
 }
 
- module.exports = forecast
+module.exports = forecast
